bbe573c21856f8a98b94e3ac775e935f2cd1d95c
Wilton <soutotech@gmail.com>
2021-11-22 22:15:21 -0300

    Fist Commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package com.dio;
+
+import com.dio.model.*;
+
+public class PrimeiroPrograma {
+	
+public static void main(String[] args) {
+	int a = 3;
+	int b = 8;
+	
+	System.out.println("Hello World" + (a*b));
+}
+}
+
+enquanto (contador<=3)
